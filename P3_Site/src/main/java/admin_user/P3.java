f8667d563a4a25b7832e8f3d3f717fef6997b663
i-Dimitri <149518219+i-Dimitri@users.noreply.github.com>
2023-12-18 21:31:13 +0200

    Add files via upload

---
+++ P3_Site/src/main/java/admin_user/P3.java
@@ -0,0 +1,15 @@
+package admin_user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P3 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(P3.class, args);
+    }
+
+}
+
+
